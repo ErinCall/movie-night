9fc05e16db0731580b9b213c3fb05c2cdac707cc
Drew Chandler <drew@kongregate.com>
2012-06-13 23:03:31 -0700

    Initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieNight::Application.config.secret_token = '0a4fda48d78d3c2eff3b766e44a8b14c68f0d9281379c9bf7bd9d972bf15b381b7088dababbaeaaa14b8d3ce4270373500ae55203438966032e0e2368cb4ee78'
