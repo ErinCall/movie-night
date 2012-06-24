46b5e13f3e0ac3da4e160f16b990cd25981818ac
Drew Chandler <drew@kongregate.com>
2012-06-24 00:04:21 -0700

    Validate runtime is less than 125 minutes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,8 @@
 class Movie < ActiveRecord::Base
   attr_accessible :rotten_tomatoes_id, :title, :year, :runtime, :thumbnail_url, :detailed_url
 
-  validates_uniqueness_of :rotten_tomatoes_id, :message => 'Movie has already been nominated'
+  validates_uniqueness_of :rotten_tomatoes_id, message: 'Movie has already been nominated'
+  validates_numericality_of :runtime, less_than_or_equal_to: 125, message: 'Movie is over the 125 minute limit'
 
   belongs_to :user
 
